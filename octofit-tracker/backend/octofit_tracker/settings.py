54a1fbf6af544d69e9c5464a0eca4fe4d3e45d12
Paweł Bajorek <43749445+ahpawel@users.noreply.github.com>
2025-05-19 22:44:59 +0000

    Setup Django REST API root endpoint for codespace and localhost URLs

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-a3@rp0!4+wl%bu2egm)d3e%i!^d6x5%sqy%=qt_$jz5i$ml-bg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', 'fictional-disco-g74wxjgv56rc5x7-8000.app.github.dev']
 
 
 # Application definition
